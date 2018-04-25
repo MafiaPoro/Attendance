ba99373536221c10214eb4f821ce1c58e7c42444
MafiaPoro <p.iacob@ymail.com>
2018-04-25 11:07:35 +0100

    Mobile friendly

--- loggedIn.php
+++ loggedIn.php
@@ -2,13 +2,15 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 	<head>
 		<title>Cross-Browser QRCode Generator</title>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-		<meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3mobile.css">
+		<!-- <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no" /> -->
 		<script type="text/javascript" src="jquery.min.js"></script>
 		<script type="text/javascript" src="qrcode.js"></script>
 	</head>
 	<body>
-		<div id="qrcode" style="width:250px; height:250px; margin-top:15px;"></div>
+		<div id="qrcode" style="width:250px; height:250px; display:inline-block;position:fixed;top:0;bottom:0;left:0;right:0;margin:auto;"></div>
 		<script type="text/javascript">
 			var qrcode = new QRCode(document.getElementById("qrcode"),
 			{
